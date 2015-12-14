74643222688b02ab86fcf568f8dca723549f2d53
Oleg Sviderskij <oleg@sviderskij.lt>
2015-12-14 12:19:54 +0200

    Service unit testst

--- src/test/java/parking/service/ParkingServiceTest.java
+++ src/test/java/parking/service/ParkingServiceTest.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import parking.beans.request.ParkingNumberRequest;
 import parking.beans.request.SetUnusedRequest;
 import parking.beans.response.ParkingLot;
 import parking.builders.LotsBuilder;
@@ -38,54 +39,22 @@ public class ParkingServiceTest {
     @Mock
     private SecurityContext mockSecurityContext;
 
+    @Mock
     private Authentication authentication;
 
 
     private List<ParkingLot> mockedParkingLotList = new ArrayList<ParkingLot>();
+    private Account mockedAccount;
     private static final String CURRENT_USER_NAME = "name";
 
     @Before
     public void initMock() {
-
-        authentication = new Authentication() {
-            @Override
-            public Collection<? extends GrantedAuthority> getAuthorities() {
-                return null;
-            }
-
-            @Override
-            public Object getCredentials() {
-                return null;
-            }
-
-            @Override
-            public Object getDetails() {
-                return null;
-            }
-
-            @Override
-            public Object getPrincipal() {
-                return null;
-            }
-
-            @Override
-            public boolean isAuthenticated() {
-                return false;
-            }
-
-            @Override
-            public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
-
-            }
-
-            @Override
-            public String getName() {
-                return CURRENT_USER_NAME;
-            }
-        };
-
+        when(authentication.getName()).thenReturn(CURRENT_USER_NAME);
         when(mockSecurityContext.getAuthentication()).thenReturn(authentication);
         SecurityContextHolder.setContext(mockSecurityContext);
+
+        mockedAccount= new Account();
+        mockedAccount.setParkingNumber(105);
 
         mockedParkingLotList.add(new LotsBuilder().number(100).owner("Name Surname").build());
         mockedParkingLotList.add(new LotsBuilder().number(101).owner("Name Surname2").build());
@@ -102,20 +71,64 @@ public class ParkingServiceTest {
 
     @Test
     public void whenOwnerFreeUpParkingLot() {
-        Account account = new Account();
-        account.setParkingNumber(105);
 
         SetUnusedRequest request = new SetUnusedRequest();
-        given(accountRepository.findByUsername(CURRENT_USER_NAME)).willReturn(account);
-
+        given(accountRepository.findByUsername(CURRENT_USER_NAME)).willReturn(mockedAccount);
         service.freeOwnersParking(request);
 
         ArgumentCaptor captor = ArgumentCaptor.forClass(SetUnusedRequest.class);
         verify(lotsRepository).freeOwnersParking((SetUnusedRequest) captor.capture());
 
         SetUnusedRequest value = (SetUnusedRequest) captor.getValue();
-        assertEquals(value.getNumber(), account.getParkingNumber());
+        assertEquals(value.getNumber(), mockedAccount.getParkingNumber());
+    }
 
+    @Test
+    public void whenCustomerDoesNotHaveParkingAssigned() {
+        mockedAccount.setParkingNumber(null);
+        SetUnusedRequest request = new SetUnusedRequest();
 
+        given(accountRepository.findByUsername(CURRENT_USER_NAME)).willReturn(mockedAccount);
+
+        service.freeOwnersParking(request);
+        verify(lotsRepository, never()).freeOwnersParking(request);
+    }
+
+    @Test
+    public void whenOwnerRecallParkingLot() {
+        given(accountRepository.findByUsername(CURRENT_USER_NAME)).willReturn(mockedAccount);
+
+        service.recallParking();
+
+        ArgumentCaptor captor = ArgumentCaptor.forClass(ParkingNumberRequest.class);
+        verify(lotsRepository).recallParking((ParkingNumberRequest) captor.capture());
+
+        ParkingNumberRequest value = (ParkingNumberRequest) captor.getValue();
+        assertEquals(value.getNumber(), mockedAccount.getParkingNumber());
+    }
+
+    @Test
+    public void whenCustomerDoesNotHaveParkingAssignedAndTryRecall() {
+
+        ArgumentCaptor captor = ArgumentCaptor.forClass(ParkingNumberRequest.class);
+        mockedAccount.setParkingNumber(null);
+        given(accountRepository.findByUsername(CURRENT_USER_NAME)).willReturn(mockedAccount);
+
+        service.recallParking();
+        verify(lotsRepository, never()).recallParking((ParkingNumberRequest) captor.capture());
+    }
+
+    @Test
+    public void whenUserReserveParkingLot() {
+        ParkingNumberRequest request = new ParkingNumberRequest();
+
+        service.reserve(request);
+        verify(lotsRepository).reserve(request, CURRENT_USER_NAME);
+    }
+
+    @Test
+    public void whenCancelReservation() {
+       service.cancelRezervation();
+       verify(lotsRepository).cancelReservation(CURRENT_USER_NAME);
     }
 }
