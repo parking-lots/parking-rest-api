f694b111d19dcc83001af87e2dcacebc6f253794
Oleg Sviderskij <oleg@sviderskij.lt>
2015-12-15 11:32:51 +0200

    Application configuration annotation

--- src/main/java/parking/Application.java
+++ src/main/java/parking/Application.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 
+@Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class Application extends SpringBootServletInitializer {
