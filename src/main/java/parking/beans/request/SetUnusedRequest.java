3c7be11662fcd29e8bab4c75091b79297552df56
Lina <Lina@Mindaugass-MacBook-Pro.local>
2016-04-22 14:53:36 +0300

    fixed parking exceptions bug

--- src/main/java/parking/beans/request/SetUnusedRequest.java
+++ src/main/java/parking/beans/request/SetUnusedRequest.java
@@ -12,10 +12,8 @@ public class SetUnusedRequest {
     private Integer number;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private Date freeFrom = new Date();
-    //@NotNull(message = "Must provide untill which date are you giving your parking away!")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private Date freeTill = new Date();
-    //private Date freeTill;
 
     public Integer getNumber() {
         return number;
