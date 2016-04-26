16425434c32369ced83a433f9b3d0aff1cee6031
Julius <burbulis.julius@gmail.com>
2016-04-26 16:07:42 +0300

    Added email, password and car info to user list response

--- src/main/java/parking/beans/response/User.java
+++ src/main/java/parking/beans/response/User.java
@@ -1,6 +1,7 @@
 package parking.beans.response;
 
 import parking.beans.document.Account;
+import parking.beans.document.Car;
 import parking.beans.document.Role;
 
 import java.util.ArrayList;
@@ -9,20 +10,25 @@ import java.util.List;
 public class User extends Response {
     private String fullName;
     private String username;
-    private List<String> roles = new ArrayList<>();
+    private String password;
+    private String email;
     private String number;
+    private List<Car> carList = new ArrayList<>();
+    private List<String> roles = new ArrayList<>();
 
     public User(Account account) {
         this.fullName = account.getFullName();
         this.username = account.getUsername();
+        this.password = account.getPassword();
+        this.email = account.getEmail();
+        this.number = (account.getParking() == null) ? "" : account.getParking().getNumber().toString();
+        this.carList = account.getCarList();
 
         if (!account.getRoles().isEmpty()) {
             for (Role role : account.getRoles()) {
                 this.roles.add(role.getName());
             }
         }
-
-        this.number = (account.getParking() == null) ? "" : account.getParking().getNumber().toString();
     }
 
     public String getFullName() {
@@ -41,6 +47,22 @@ public class User extends Response {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getNumber() {
         return number;
     }
@@ -49,11 +71,19 @@ public class User extends Response {
         this.number = number;
     }
 
+    public List<Car> getCarList() {
+        return carList;
+    }
+
+    public void setCarList(List<Car> carList) {
+        this.carList = carList;
+    }
+
     public List<String> getRoles() {
         return roles;
     }
 
-    public void setRoles(List<String> hasRoles) {
-        this.roles = hasRoles;
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
     }
 }
