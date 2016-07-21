a00f341e7f6935ac423f1e842e2d6695522cd33e
Lina <Lina@Mindaugass-MacBook-Pro.local>
2016-07-21 10:20:42 +0300

    made code more readable

--- src/main/java/parking/service/TaskService.java
+++ src/main/java/parking/service/TaskService.java
@@ -28,29 +28,33 @@ public class TaskService {
         List<Log> logRecords = logRepository.findDailyConfirmations(ActionType.EMAIL_CONFIRMED, new Date());
 
         int numberOfUsers = logRecords.size();
-        if (numberOfUsers > 0) {
-            String subject;
-            String message;
-            switch (numberOfUsers) {
-                case 1:
-                    subject = "Today " + logRecords.size() + " new Parkinger user registered";
-                    message = "<p>Hello,</p><p>There is " + logRecords.size() + " new user registered at Parkinger waiting for your approval.</p>" +
-                            "<p>Please, register user's car plate numbers and activate the account.</p>" +
-                            "<p>You can check new users <a href=\"http://www.parkinger.net/admin\">here</a></p>";
-                    break;
-                default:
-                    subject = "Today " + logRecords.size() + " new Parkinger users registered";
-                    message = "<p>Hello,</p><p>There are " + logRecords.size() + " new users registered at Parkinger waiting for your approval.</p>" +
-                            "<p>Please, register their car plate numbers and activate their accounts.</p>" +
-                            "<p>You can check new users <a href=\"http://www.parkinger.net/admin\">here</a></p>";
-                    break;
-            }
+        if (numberOfUsers == 0) {
+            return;
+        }
 
-            try {
-                mailService.sendEmail("lina.po@outlook.com", subject, message);
-            } catch (Exception e) {
-                throw exceptionHandler.handleException(ExceptionMessage.COULD_NOT_SEND_EMAIL, httpRequest);
-            }
+        String subject;
+        String message;
+
+        switch (numberOfUsers) {
+            case 1:
+                subject = "Today " + logRecords.size() + " new Parkinger user registered";
+                message = "<p>Hello,</p><p>There is " + logRecords.size() + " new user registered at Parkinger waiting for your approval.</p>" +
+                        "<p>Please, register user's car plate numbers and activate the account.</p>" +
+                        "<p>You can check new users <a href=\"http://www.parkinger.net/admin\">here</a></p>";
+                break;
+            default:
+                subject = "Today " + logRecords.size() + " new Parkinger users registered";
+                message = "<p>Hello,</p><p>There are " + logRecords.size() + " new users registered at Parkinger waiting for your approval.</p>" +
+                        "<p>Please, register their car plate numbers and activate their accounts.</p>" +
+                        "<p>You can check new users <a href=\"http://www.parkinger.net/admin\">here</a></p>";
+                break;
+        }
+
+        try {
+            mailService.sendEmail("lina.po@outlook.com", subject, message);
+        } catch (Exception e) {
+            throw exceptionHandler.handleException(ExceptionMessage.COULD_NOT_SEND_EMAIL, httpRequest);
         }
     }
+
 }
